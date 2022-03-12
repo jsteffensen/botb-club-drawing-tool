ee5b9a15ddd4076ceb3d47f2a2561923b08bbb8b
jsteffensen <jsteffensen@users.noreply.github.com>
2022-03-12 22:52:18 +0100

    initial commit

---
+++ drawing-tool.js
@@ -0,0 +1,10 @@
+var imageWidth = 4416;
+var imageHeight = 3336;
+var imageRatio = 0.2;
+
+var svgDC = d3.select('#svg-dc')
+  .attr('width', imageWidth * imageRatio)
+  .attr('height', imageHeight * imageRatio)
+  .attr('style', 'cursor:none;margin:20px;')
+  .style('background', 'url("https://botb-club.com/wp-content/uploads/2022/03/dc1022.jpg.jpg") no-repeat')
+  .style('background-size', 'cover');
