952d63d1f7bd40330b4cd315de5c270ed65af7d4
jsteffensen <jsteffensen@users.noreply.github.com>
2022-03-12 23:58:03 +0100

    console log

--- drawing-tool.js
+++ drawing-tool.js
@@ -6,7 +6,7 @@ var svgDC = d3.select('#svg-dc')
   .attr('width', imageWidth * imageRatio)
   .attr('height', imageHeight * imageRatio)
   .attr('style', 'cursor:none;margin:20px;')
-  .style('background', 'url("https://botb-club.com/wp-content/uploads/2022/03/dc1022.jpg.jpg") no-repeat')
+  .style('background', 'url(https://botb-club.com/wp-content/uploads/2022/03/dc1022.jpg) no-repeat')
   .style('background-size', 'cover');
   
 console.log(svgDC);
